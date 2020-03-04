f1e0bcdf27c5d7580796201545e37c58b8021de0
NguyenManh <nguyenmanh@nguyenmanh.local>
2020-03-04 12:50:03 +0700

    first commit

---
+++ js/initFirebase.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAfEII77p9d0TLeQUd-lUEfGVGogXd07JQ",
+    authDomain: "white-inscriber-222017.firebaseapp.com",
+    databaseURL: "https://white-inscriber-222017.firebaseio.com",
+    projectId: "white-inscriber-222017",
+    storageBucket: "white-inscriber-222017.appspot.com",
+    messagingSenderId: "778081566865"
+};
+firebase.initializeApp(config);
